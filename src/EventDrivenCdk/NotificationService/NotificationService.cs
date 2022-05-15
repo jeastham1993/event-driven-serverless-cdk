727c7aaf2f1d682596693b795f78861f7e40557c
James Eastham <jamesuk@amazon.com>
2022-05-15 17:04:43 +0100

    Add emails for positive and negative reviews

---
+++ src/EventDrivenCdk/NotificationService/NotificationService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Amazon.CDK.AWS.Events;
+using Amazon.CDK.AWS.Events.Targets;
+using Amazon.CDK.AWS.Sagemaker;
+using Amazon.CDK.AWS.StepFunctions;
+using Amazon.CDK.AWS.StepFunctions.Tasks;
+using Constructs;
+using Newtonsoft.Json;
+using EventBus = Amazon.CDK.AWS.Events.EventBus;
+
+namespace EventDrivenCdk
+{
+    public class NotificationServiceProps
+    {
+        public EventBus CentralEventBus { get; set; }    
+    }
+    
+    public class NotificationService : Construct
+    {
+        public NotificationService(Construct scope, string id, NotificationServiceProps props) : base(scope, id)
+        {
+            var choice = new Choice(this, "EventTypeChoice")
+                .When(Condition.StringEquals(JsonPath.StringAt("$.detail.type"), "positiveReview"),
+                    WorkflowManager.SendEmail(this, "SendPositiveEmail", new SendEmailProps()
+                    {
+                        To = JsonPath.StringAt("$.detail.emailAddress"),
+                        Subject = "Thankyou for your review",
+                        Body = "Thankyou for your positive review",
+                        UniqueIdentifier = "SendPositiveEmail"
+                    }))
+                .When(Condition.StringEquals(JsonPath.StringAt("$.detail.type"), "negativeReview"),
+                    WorkflowManager.SendEmail(this, "SendNegativeEmail", new SendEmailProps()
+                    {
+                        To = JsonPath.StringAt("$.detail.emailAddress"),
+                        Subject = "Sorry!",
+                        Body =
+                            "I'm sorry our product didn't meet your satisfaction. One of our customer service agents will be in touch shortly",
+                        UniqueIdentifier = "SendNegativeEmail"
+                    }));
+            
+            var stateMachine = new StateMachine(this, "NotificationServiceStateMachine", new StateMachineProps
+            {
+                Definition = choice,
+                StateMachineType = StateMachineType.STANDARD
+            });
+
+            var rule = new Rule(this, "NotificationRule", new RuleProps()
+            {
+                EventBus = props.CentralEventBus,
+                RuleName = "NotificationRule",
+                EventPattern = new EventPattern()
+                {
+                    DetailType = new string[2] {"positive-review", "negative-review"},
+                    Source = new string[1] {"event-driven-cdk.sentiment-analysis"},
+                },
+                Targets = new IRuleTarget[1]
+                {
+                    new SfnStateMachine(stateMachine)
+                }
+            });
+            
+        }
+    }
+}
---
+++ src/EventDrivenCdk/NotificationService/SendEmail.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using Amazon.CDK.AWS.StepFunctions;
+using Amazon.CDK.AWS.StepFunctions.Tasks;
+using Constructs;
+
+namespace EventDrivenCdk
+{
+    public class SendEmailProps
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string UniqueIdentifier { get; set; }
+    }
+    
+    public static class WorkflowManager
+    {
+        public static CallAwsService SendEmail(Construct scope, string id, SendEmailProps props)
+        {
+            return new CallAwsService(
+                scope, props.UniqueIdentifier,
+                new CallAwsServiceProps()
+                {
+                    Service = "ses",
+                    Action = "sendEmail",
+                    Parameters = new Dictionary<string, object>(2)
+                    {
+                        {
+                            "Destination", new Dictionary<string, object>()
+                            {
+                                {"ToAddresses", JsonPath.Array(props.To)}
+                            }
+                        },
+                        {"Source", "jamesuk@amazon.co.uk"},
+                        {
+                            "Message", new Dictionary<string, object>()
+                            {
+                                {
+                                    "Body", new Dictionary<string, object>()
+                                    {
+                                        {
+                                            "Html", new Dictionary<string, object>()
+                                            {
+                                                {"Charset", "UTF-8"},
+                                                {
+                                                    "Data",
+                                                    $"<html><head></head><body><p>{props.Body}</p></body></html>"
+                                                }
+                                            }
+                                        },
+                                        {
+                                            "Text", new Dictionary<string, object>()
+                                            {
+                                                {"Charset", "UTF-8"},
+                                                {"Data", props.Body}
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "Subject", new Dictionary<string, object>()
+                                    {
+                                        {"Data", props.Subject},
+                                        {"Charset", "UTF-8"},
+                                    }
+                                },
+                            }
+                        }
+                    },
+                    IamResources = new string[1] {"*"},
+                });
+        }
+    }
+}
