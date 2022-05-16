2d3ae7a4e2626c6c3a553c837527af0d1bb98a43
James Eastham <jamesuk@amazon.com>
2022-05-16 09:16:15 +0100

    Add console app for customer interactions

---
+++ src/CustomerInteractionManager/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text.Json;
+using Amazon.StepFunctions;
+using Amazon.StepFunctions.Model;
+using CustomerInteractionManager;
+
+var client = new AmazonStepFunctionsClient();
+
+Console.WriteLine("What would you like to do?");
+Console.WriteLine("1) Claim a case waiting action?");
+Console.WriteLine("2) Mark a claim complete?");
+var option = Console.ReadLine();
+
+var taskToken = string.Empty;
+var output = string.Empty;
+
+if (option == "1")
+{
+    Console.WriteLine("Task token?");
+    taskToken = Console.ReadLine();
+    Console.WriteLine("Who are you?");
+    var claimedBy = Console.ReadLine();
+
+    output = JsonSerializer.Serialize(new ClaimedByTaskResult(claimedBy));
+}
+else if (option == "2")
+{
+    
+}
+
+var result = client.SendTaskSuccessAsync(new SendTaskSuccessRequest()
+{
+    TaskToken = taskToken,
+    Output = output
+}).Result;
