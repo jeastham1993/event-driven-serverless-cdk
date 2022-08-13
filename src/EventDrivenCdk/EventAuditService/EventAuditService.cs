d76be7bb1e987a8f75354a0511c7f2e71ea1bd82
James Eastham <jamesuk@amazon.com>
2022-08-13 17:52:28 +0100

    Add API to audit service

--- src/EventDrivenCdk/EventAuditService/EventAuditService.cs
+++ src/EventDrivenCdk/EventAuditService/EventAuditService.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using Amazon.CDK;
+using Amazon.CDK.AWS.APIGateway;
 using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.StepFunctions;
 using Amazon.CDK.AWS.StepFunctions.Tasks;
 using Constructs;
@@ -40,12 +43,40 @@ namespace EventDrivenCdk.EventAuditService
                 }
             });
 
+            // Simple workflow to take event bridge input and store in dynamodb.
+            var queryWorkflow = WorkflowStep.QueryDynamo(this)
+                .Next(new Map(this, "LoopItems", new MapProps()
+                {
+                    ItemsPath = "$.QueryResult.Items"
+                }).Iterator(WorkflowStep.FormatResponse(this)));
+
             var stateMachine =
                 new DefaultStateMachine(this, "EventAuditStateMachine", sfnWorkflow, StateMachineType.EXPRESS);
+
+            var apiStateMachine = new DefaultStateMachine(this, "EventAuditApiStateMachine", queryWorkflow, StateMachineType.EXPRESS);
+
+            apiStateMachine.AddToRolePolicy(new PolicyStatement(new PolicyStatementProps()
+            {
+                Actions = new string[1] { "dynamodb:Query" },
+                Resources = new string[1] { auditTable.TableArn }
+            }));
+
+            // Create the API
+            var queryApi = new StepFunctionsRestApi(this, "EventAuditQueryApi", new StepFunctionsRestApiProps
+            {
+                StateMachine = apiStateMachine,
+            });
 
             // Add rule to the central event bus.
             CentralEventBus.AddRule(this, "EventAuditRule",
                 new string[4] {"event-driven-cdk.api", "event-driven-cdk.sentiment-analysis", "event-driven-cdk.notifications", "event-driven-cdk.customer-service"}, stateMachine);
+
+            var output = new CfnOutput(this, "AuditApiEndpoint", new CfnOutputProps()
+            {
+                ExportName = "AuditAPIEndpoint",
+                Description = "The endpoint for the created audit API",
+                Value = queryApi.Url
+            });
         }
     }
 }
